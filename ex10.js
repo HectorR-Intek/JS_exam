c00ef1c16e39e48dcc5605bea8703aa55d25cd0c
HectorR-Intek <hjrodriguez@intekglobal.com>
2025-09-04 17:07:55 -0600

    GET request is now available to Person object

--- ex10.js
+++ ex10.js
@@ -1,50 +1,57 @@
 "use strict";
-function Person(first, last){                           //Constructor function for Person object.
-    this.firstName = first;
-    this.lastName = last;
+function Person(first, last) {
+  //Constructor function for Person object.
+  this.firstName = first;
+  this.lastName = last;
 }
 
-Person.prototype.greet = function(){                    // We add a method called greet.
-    console.log(this.firstName, this.lastName);         // This method belongs to the prototype so not every instance has a copy.
-}                                                       // They all use the same.
+Person.prototype.greet = function () {
+  // We add a method called greet.
+  console.log(this.firstName, this.lastName); // This method belongs to the prototype so not every instance has a copy.
+}; // They all use the same.
 
-function Medic(firstName, lastName, specialty){
-    Person.call(this, firstName, lastName);             //This line inherits PROPERTIES from Person INSTANCE.
-    this.specialty = specialty;                         //This line adds a new property.
+function Medic(firstName, lastName, specialty) {
+  Person.call(this, firstName, lastName); //This line inherits PROPERTIES from Person INSTANCE.
+  this.specialty = specialty; //This line adds a new property.
 }
 
-Medic.prototype = Object.create(Person.prototype)       //This line inherits methods from Person
+Medic.prototype = Object.create(Person.prototype); //This line inherits methods from Person
 Medic.prototype.constructor = Medic;
 
-Medic.prototype.introduce = function(){
-    console.log(this.firstName+" "+this.lastName+" "+this.specialty);
-}
+Medic.prototype.introduce = function () {
+  console.log(this.firstName + " " + this.lastName + " " + this.specialty);
+};
 
+/*
 Medic.prototype.generateQuery = function (){
     return new URLSearchParams({
         firstName : this.firstName,
         lastName  : this.lastName,
         specialty : this.specialty,
     }).toString();
-}
+}*/
 
-Medic.prototype.sendRequest = function(){
-    const url = `https://api.doctors.com/info?${this.generateQuery()}`;
+Person.prototype.generateQuery = function () {
+  return new URLSearchParams({
+    firstName: this.firstName,
+    lastName: this.lastName,
+  }).toString();
+};
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => console.log("Data retrieved:", data))
-        .catch(err => console.error("Error:", err));
-}
+Person.prototype.sendRequest = function () {
+  const url = `https://api.person.com/info?${this.generateQuery()}`;
 
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log("Data retrieved:", data))
+    .catch((err) => console.error("Error:", err));
+};
 
 var renata = new Person("Renata", "Vazquez");
 var drRenata = new Medic("Renata", "Vazquez", "Cardiology");
 
-
 renata.greet();
 
-drRenata.introduce()
-drRenata.greet();       //Output of this line equals Renata.greet, yet this is a Medic object
-                        //thus proving the inheritance occured.
-             
+drRenata.introduce();
+drRenata.greet(); //Output of this line equals Renata.greet, yet this is a Medic object
+//thus proving the inheritance occured.
