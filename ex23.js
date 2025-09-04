cb5552250c69b0c0c82e1ac4927d34dd98055663
HectorR-Intek <hjrodriguez@intekglobal.com>
2025-09-04 17:19:30 -0600

    Key is now optional and no more collisions

--- ex23.js
+++ ex23.js
@@ -1,54 +1,69 @@
 "use strict";
-class RandomGenerator{
-    constructor(min, max, key){                // key is an arbitrary number that changes the pseudorandom generation.
-        if(min>max){
-            throw new Error("Max has to be greater than min");
-        }
-    
+
+class RandomGenerator {
+  //key now has a default value in case user does not provide one
+  constructor(min, max, key = 0x9e3779b9) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+      throw new Error("Max and min must be integer numbers.");
+    }
+    if (min > max) throw new Error("Max must be greater than min");
+
     this.min = min;
     this.max = max;
     this.size = max - min + 1;
-    this.key = key >>> 0;                     // This line converts type to a 32bits unsigned int
-    this.counter = 0;                         // All these values are preserved by closure.
+    this.key = key >>> 0;
+    this.counter = 0;
 
-    const bits = Math.ceil(Math.log2(this.size));  // These are the number of bits needed to represent the number `size`.
-    this.halfBits = Math.floor(bits/2);
-    this.mask = (1<< this.halfBits) - 1;      // These are halfbits filled with ones, since << displaces the one, halfbits to the left
-    }                                         // and then the substraction of 1 shifts the zeroes to ones.
-    
-    simpleFeistel(index){                     // Index will be the seed for the pseudorandom generation  
-        let L = index & this.mask;                               //    
-        let R = (index >> this.halfBits) & this.mask;
+    const bits = Math.ceil(Math.log2(this.size));
+    this.halfBits = Math.ceil(bits / 2); //  CEIL instead of floor
+    this.mask = (1 << this.halfBits) - 1;
+  }
 
-        let F = Math.imul((R^this.key), 0x5bd1e995)& this.mask;  // imul is a multiplication with a big integer and & keeps only 
-                                                                 // halfBits of the product
-        let newL = R;
-        let newR = L ^ F;
+  coreFeistel(x) {
+    let L = x & this.mask;
+    let R = (x >>> this.halfBits) & this.mask;
 
-        let result = (newR << this.halfBits) | newL;
+    const F = Math.imul((R ^ this.key) >>> 0, 0x5bd1e995) & this.mask;
 
-        if(result >= this.size){
-            return this.simpleFeistel((result+1) % this.size) // In case the operations yield a number outside of size.
-        }
-        return result;
-    }
+    const newL = R;
+    const newR = (L ^ F) & this.mask;
 
-    next(){
-        if(this.counter >= this.size) return null;      // Numbers in range have all been yielded and no more are left
-        const val = this.simpleFeistel(this.counter++);
-        return val + this.min;
-    }
+    return ((newR << this.halfBits) | newL) >>> 0;
+  }
 
-    reset(){
-        this.counter = 0;
-    }
+  permute(index) {
+    let x = index;
+    do {
+      x = this.coreFeistel(x);
+    } while (x >= this.size);
+    return x;
+  }
+
+  next() {
+    if (this.counter >= this.size) return null;
+    const val = this.permute(this.counter++);
+    return this.min + val;
+  }
+
+  reset() {
+    this.counter = 0;
+  }
+
+  hasNext() {
+    return this.counter < this.size;
+  }
 }
 
 // This is a particular instance of the generator being called ten times.
 
-const gen = new RandomGenerator(10, 25, 34335);
+const gen = new RandomGenerator(1, 10, 34335);
 
-for(let i=0; i<10; i++){
-    console.log(gen.next());
+for (let i = 0; i < 10; i++) {
+  console.log(gen.next());
 }
 
+const gen2 = new RandomGenerator(1, 10);
+
+for (let i = 0; i < 10; i++) {
+  console.log(gen2.next());
+}
